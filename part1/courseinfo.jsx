c7e4fd61af4b6b0c74e566e4d5df6bb4212ea2b8
youssef gamal <115406392+yooousef@users.noreply.github.com>
2025-08-28 22:13:34 +0300

    courseinfo.jsx
    split Content into multiple Part components and working with props.

---
+++ part1/courseinfo.jsx
@@ -0,0 +1,49 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>
+}
+
+const Part = (props) => {
+  return <p>{props.part} {props.exercise}</p>
+}
+
+const Content = (props) => {
+  return(
+    <>
+     <Part part={props.part1} exercises={props.ex1} />
+      <Part part={props.part2} exercises={props.ex2} />
+      <Part part={props.part3} exercises={props.ex3} />
+    </>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises {props.ex1 + props.ex2 + props.ex3}
+    </p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <>
+      <Header course = {course} />
+      <Content 
+        part1={part1} ex1={exercises1}
+        part2={part2} ex2={exercises2}
+        part3={part3} ex3={exercises3}
+      />
+      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3} />
+    </>
+  )
+}
+
+export default App
