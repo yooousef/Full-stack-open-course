ba149b6c62aa69ea661e8758fd124eb2f8880dd5
youssef gamal <115406392+yooousef@users.noreply.github.com>
2025-09-03 04:08:30 +0300

    1.10.jsx

---
+++ part1/unicafe/1.10.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>{text}</button>
+)
+
+const StatisticLine = ({ text, value }) => (
+  <p>{text}: {value}</p>
+)
+
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad
+
+  if (all === 0) {
+    return <p>No feedback given</p>
+  }
+
+  const average = (good - bad) / all
+  const positive = ((good / all) * 100).toFixed(1) + " %"
+
+  return (
+    <div>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={all} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="positive" value={positive} />
+    </div>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
+
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+export default App
