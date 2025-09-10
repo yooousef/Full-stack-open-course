eb489b403b143ec96d9cae56dd5f04948c497cc2
youssef gamal <115406392+yooousef@users.noreply.github.com>
2025-09-10 04:26:14 +0300

    Create App.jsx

---
+++ part2/2.14/App.jsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react"
+import PersonForm from "./components/PersonForm"
+import Persons from "./components/Persons"
+import Filter from "./components/Filter"
+import personService from "./services/persons"
+
+const App = () => {
+   const [persons, setPersons] = useState([])
+   
+   useEffect(() => {
+    personService.getAll()
+      .then(initialPerson => setPersons(initialPerson))
+  }, [])
+
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filterName, setFilterName] = useState('')
+
+  const handleNewName = (event) => setNewName(event.target.value)
+  const handleNewNumber = (event) => setNewNumber(event.target.value)
+  const handleFilter = (event) => setFilterName(event.target.value)
+
+  const addNewPerson = (event) => {
+    event.preventDefault()
+    const nameObject = { name: newName, number: newNumber }
+
+    const exists = persons.some(person => person.name === nameObject.name)
+    if (!exists) {
+      personService.create(nameObject)
+        .then(newPerson => setPersons(persons.concat(newPerson)))
+
+    } else {
+      alert(`${nameObject.name} is already added to phonebook`)
+    }
+
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const personsToShow = persons.filter(person =>
+    person.name.toLowerCase().includes(filterName.toLowerCase())
+  )
+
+ const deletePerosn = (id, name) => {
+  if(window.confirm(`Delete ${name}?`)){
+    personService.remove(id)
+    .then(() => {
+      setPersons(person.filter(p => p.id !== id ))
+    })
+    .catch(error => {
+      alert(`The person ${name} was already removed from server`)
+       setPersons(persons.filter(p => p.id !== id))
+    })
+  }
+ }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <Filter filterName={filterName} handleFilter={handleFilter} />
+
+      <h3>Add a new</h3>
+
+      <PersonForm 
+        addNewPerson={addNewPerson}
+        newName={newName}
+        handleNewName={handleNewName}
+        newNumber={newNumber}
+        handleNewNumber={handleNewNumber}
+      />
+
+      <h3>Numbers</h3>
+
+      <Persons personsToShow={personsToShow} deletePerosn = {deletePerosn}/>
+    </div>
+  )
+}
+
+export default App
