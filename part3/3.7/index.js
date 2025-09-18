5349815dc7d175d0ab7e9d5315251a4c4e5a2154
youssef gamal <115406392+yooousef@users.noreply.github.com>
2025-09-19 02:33:04 +0300

    Create index.js

---
+++ part3/3.7/index.js
@@ -0,0 +1,86 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+app.use(express.json())
+app.use(morgan('tiny'))
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (req, res) => {
+res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  person = persons.find(person => person.id === id)
+
+  person ? res.json(person) : res.status(404).end()
+})
+
+app.delete('/api/persons/:id', (req, res) => 
+{
+  const id = req.params.id
+  persons = persons.filter(person => person.id !== id)
+  res.status(204).end()
+})
+
+app.get('/info', (req, res) => {
+  const info = `<p>Phonebook has info for ${persons.length} people</p>
+  <p>${new Date()}</p>`;
+  res.send(info);
+})
+
+const generateId = ()=> {
+  const maxId = persons.length > 0 ? Math.max(...persons.map(p => Number(p.id))) : 0
+  return String(maxId + 1)
+}
+
+app.post('/api/persons', (req, res) =>{
+  const body = req.body
+  const exist = persons.find(p => p.name === body.name)
+  if(!body.name || !body.number){
+    return res.status(400).json({
+      error: "The name or the number is missing."
+    })
+  }
+  else if(exist){
+    return res.status(400).json({
+      error: "The same name is already existed. Try another one!"
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()  
+  }
+  persons = persons.concat(person)
+  res.json(person)
+})
+
+app.listen(3001, () => {
+console.log('Server running on port 3001')
+})
+
